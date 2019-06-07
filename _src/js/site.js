c3ec04e249c7c0cb409907b1cc9fb950d23aa509
Keith Bartholomew <keith.bartholomew@rackspace.com>
2019-06-07 15:26:15 -0500

    remove unused things pt 1

--- _src/js/site.js
+++ _src/js/site.js
@@ -1,31 +1,17 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-if(document.getElementById('year')) {
-  var ThisYear = require('./components/this-year');
-  ReactDOM.render(<ThisYear />, document.getElementById('year'));
-}
-
-if(document.getElementById('colophon-link')) {
-  var ColophonLink = require('./components/colophon-link');
-  ReactDOM.render(<ColophonLink />, document.getElementById('colophon-link'));
-}
-
-if(document.getElementById('contact-form')) {
+if (document.getElementById('contact-form')) {
   var ContactForm = require('./components/contact-form');
   ReactDOM.render(<ContactForm />, document.getElementById('contact-form'));
 }
 
-if(document.getElementById('contact-trigger')) {
+if (document.getElementById('contact-trigger')) {
   var ContactFormTrigger = require('./components/contact-form-trigger');
-  ReactDOM.render(<ContactFormTrigger />, document.getElementById('contact-trigger'));
-}
-
-if(document.querySelector('.work-medias')) {
-  var WorkItemList = require('./components/work-item-list');
-  var workData = JSON.parse(document.querySelector('#work-data').textContent);
-
-  ReactDOM.render(<WorkItemList data={workData} />, document.querySelector('.work-medias'));
+  ReactDOM.render(
+    <ContactFormTrigger />,
+    document.getElementById('contact-trigger')
+  );
 }
 
 var Modal = require('./components/modal');
@@ -33,7 +19,3 @@ var Modal = require('./components/modal');
 var modalContainer = document.createElement('div');
 document.body.appendChild(modalContainer);
 ReactDOM.render(<Modal />, modalContainer);
-
-if(document.body.clientHeight > window.innerHeight + 120) {
-  document.body.classList.remove('fixed-footer');
-}
