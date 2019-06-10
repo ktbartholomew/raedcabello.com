5cb0226fb1b7dbb7b56b68e46b682911a59a0ffd
Keith Bartholomew <keith@keithbartholomew.com>
2019-06-10 09:45:53 -0500

    Trim down unused code (#15)
    * remove unused things pt 1
    
    * delete more things
    
    * remove even more

--- _src/js/actions/submit-contact-form.js
+++ _src/js/actions/submit-contact-form.js
@@ -1,20 +1,20 @@
-var Q = require('q');
 var request = require('superagent');
 
 var CONTACT_INBOX = 'raedcabello@gmail.com';
 
-module.exports = function (data) {
-  return Q.Promise(function (resolve, reject) {
-    request.post('https://formspree.io/' + CONTACT_INBOX)
-    .set('X-Requested-With', 'xmlhttprequest')
-    .send('_replyto=' + encodeURIComponent(data.email))
-    .send('message=' + encodeURIComponent(data.message))
-    .end(function (err, res) {
-      if (err) {
-        return reject(err);
-      }
+module.exports = function(data) {
+  return new Promise(function(resolve, reject) {
+    request
+      .post('https://formspree.io/' + CONTACT_INBOX)
+      .set('X-Requested-With', 'xmlhttprequest')
+      .send('_replyto=' + encodeURIComponent(data.email))
+      .send('message=' + encodeURIComponent(data.message))
+      .end(function(err, res) {
+        if (err) {
+          return reject(err);
+        }
 
-      return resolve(res);
-    });
+        return resolve(res);
+      });
   });
 };
